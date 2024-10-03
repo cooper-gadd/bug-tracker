cf0a99cd46d0bea458c8b61573284e34e81d9ae7
cgadd21 <ctg7866@g.rit.edu>
2024-10-03 15:12:01 -0400

    title

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -7,7 +7,7 @@ export default function App() {
         <div>
           <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
           <p className="text-muted-foreground">
-            Here&apos;s a list of your tasks for this month!
+            Here&apos;s a list of your bugs for Whatever!
           </p>
         </div>
         <div className="flex items-center space-x-2">
