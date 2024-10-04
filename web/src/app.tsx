2d786e7c1ad415222ebea385339a35b4e736ff93
cgadd21 <ctg7866@g.rit.edu>
2024-10-04 00:58:39 -0400

    cleaning up

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -64,7 +64,7 @@ export default function App() {
   ];
 
   return (
-    <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
+    <div className="h-full flex-1 flex-col space-y-8 p-8">
       <div className="flex items-center justify-between space-y-2">
         <div>
           <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
