9d1cb15f3fdd17af953c4b612e181b05e14343c7
cgadd21 <ctg7866@g.rit.edu>
2024-10-04 21:48:11 -0400

    fixed cal styles

--- web/src/components/bug/bug-form.tsx
+++ web/src/components/bug/bug-form.tsx
@@ -266,7 +266,7 @@ export function BugForm() {
                         <Button
                           variant={"outline"}
                           className={cn(
-                            "w-[240px] pl-3 text-left font-normal",
+                            "text-left font-normal",
                             !field.value && "text-muted-foreground",
                           )}
                         >
