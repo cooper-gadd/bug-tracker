292d0161463cdd77125ffb5c5790a07301ff9a0f
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-10-23 23:53:38 -0400

    morals

--- apps/api/controller.php
+++ apps/api/controller.php
@@ -1,14 +1,21 @@
 <?php
 
-include "db.php";
-
 class Controller
 {
   private $db;
 
   public function __construct()
   {
-    $this->db = (new DB())->getConnection();
+    try {
+      $this->db = new PDO(
+        "mysql:host=" . $_SERVER["DB_HOST"] . ";dbname=" . $_SERVER["DB_NAME"],
+        $_SERVER["DB_USER"],
+        $_SERVER["DB_PASS"]
+      );
+      $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+      die("Connection failed: " . $e->getMessage());
+    }
   }
 
   public function getUsers(): void
