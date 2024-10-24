9d57b1f5884d2d0e79da32acfdcf77801e343ea4
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-10-23 22:54:11 -0400

    refactoring

--- apps/api/router.php
+++ apps/api/router.php
@@ -6,38 +6,42 @@ $requestUri = $_SERVER["REQUEST_URI"];
 
 $usersController = new UsersController();
 
-if (
-  $requestUri === "/~ctg7866/ISTE341/bug-tracker/api/users" &&
-  $requestMethod === "GET"
-) {
-  $usersController->getUsers();
-} elseif (
-  $requestUri === "/~ctg7866/ISTE341/bug-tracker/api/users" &&
-  $requestMethod === "POST"
-) {
-  $usersController->createUser($_POST);
-} elseif (
-  preg_match(
-    "/\/~ctg7866\/ISTE341\/bug-tracker\/api\/users\/(\d+)/",
-    $requestUri,
-    $matches
-  ) &&
-  $requestMethod === "GET"
-) {
-  $userId = $matches[1];
-  $usersController->getUser($userId);
-} elseif (
-  preg_match(
-    "/\/~ctg7866\/ISTE341\/bug-tracker\/api\/users\/(\d+)/",
-    $requestUri,
-    $matches
-  ) &&
-  $requestMethod === "DELETE"
-) {
-  $userId = $matches[1];
-  $usersController->deleteUser($userId);
-} else {
-  http_response_code(404);
-  echo json_encode(["message" => "Endpoint not found"]);
+$baseUri = "/~ctg7866/ISTE341/bug-tracker/api";
+
+function route(string $uriPattern, string $method, callable $callback): void
+{
+  global $baseUri, $requestUri, $requestMethod;
+
+  $pattern =
+    "@^" .
+    preg_replace("/\{[^\}]+\}/", "([^/]+)", $baseUri . $uriPattern) .
+    "$@";
+  if (
+    preg_match($pattern, $requestUri, $matches) &&
+    $requestMethod === $method
+  ) {
+    array_shift($matches);
+    $callback(...$matches);
+    exit();
+  }
 }
+
+route("/users", "GET", function () use ($usersController) {
+  $usersController->getUsers();
+});
+
+route("/users", "POST", function () use ($usersController) {
+  $usersController->createUser($_POST);
+});
+
+route("/users/{id}", "GET", function ($userId) use ($usersController) {
+  $usersController->getUser((int) $userId);
+});
+
+route("/users/{id}", "DELETE", function ($userId) use ($usersController) {
+  $usersController->deleteUser((int) $userId);
+});
+
+http_response_code(404);
+echo json_encode(["message" => "Endpoint not found"]);
 ?>
